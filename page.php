0b9fad64d405e73d43869887b730ec251a6a9d2b
John Gaffney <john.gaffney@pagely.com>
2020-11-08 17:05:15 -0700

    theme files

---
+++ page.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<p style="margin: 0 calc(10% + 60px);">
+			Tada!!!
+		</p>
+
+		<p style="margin: 0 calc(10% + 60px);">
+			<img src="https://media.giphy.com/media/An95xQZRS1B1S/source.gif" />
+		</p>
+
+			<?php
+
+			// Start the Loop.
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
